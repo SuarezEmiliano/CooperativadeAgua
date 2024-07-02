3ac40cfe9f92823af30bc96bcacaf23a051ac89d
Suarez <fabriiit@gmail.com>
2024-07-02 16:52:45 -0300

    Cooperativa de Agua

---
+++ tailwind.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				customHoverColor: '#D97425',
+		},
+	},
+	plugins: [],
+}}
